c314b16f52892cd7ad62333a14ea85beb13297ca
Luciano Mammino <lmammino@oryzone.com>
2014-03-30 15:42:31 +0200

    fixes installer

--- README.md
+++ README.md
@@ -3,26 +3,28 @@ cube-daemons
 
 This repository will help you to install [cube](https://github.com/square/cube) on an Ubuntu/Debian machine.
 
-It assumes you've already installed [node.js](http://nodejs.org/) and [npm](https://www.npmjs.org/) and that your node prefix is `/usr`.
+It assumes you've already installed [node.js](http://nodejs.org/), [npm](https://www.npmjs.org/) and [mongo](https://www.mongodb.org/) and that your node prefix is `/usr`.
 
 
 ## Install
 
-Just clone the repository with `git clone git@github.com:lmammino/cube-daemons.git` then run `sudo cube-daemons/install.sh` and it will do all the **hard work** for you.
+Just clone the repository with `git clone https://github.com/lmammino/cube-daemons.git` then run `sudo cube-daemons/install.sh` and it will do all the **hard work** for you.
 
 The hard work means:
 
  - Install cube as global package
  - Creates a dedicate `cube` user that will be used to run the daemons
  - Copies the daemons start/stop file
+ - Adds daemons as auto-starting services
  - Starts the daemons
 
 
 ## TODOs
 
- - [ ] Tests
+ - [X] Tests
  - [ ] Allow to use a customized node prefix
  - [ ] Allow to provide specific configuration for the daemons
+ - [ ] Uninstaller
 
 ## License
 
--- install.sh
+++ install.sh
@@ -1,28 +1,65 @@
 #!/bin/bash
 
-#run as sudo
+#should be run as sudo
+
+SOURCE=$(dirname ${BASH_SOURCE[0]})
 
 #install cube
-echo "\n\n====\n1. Installing cube...\n====\n\n"
+cat << EOF
+
+------------------
+1. Installing cube
+------------------
+
+EOF
 npm install --global cube
 
 #create cube user and group
-echo "\n\n====\n2. Creating cube user...\n====\n\n"
+cat << EOF
+
+---------------------
+2. Creating cube user
+---------------------
+
+EOF
 useradd -r -s /bin/false cube
 
 #create cube run dir (for pid files)
-echo "\n\n====\n3. Creating /var/run/cube folder...\n====\n\n"
+cat << EOF
+
+--------------------------------
+3. Creating /var/run/cube folder
+--------------------------------
+
+EOF
 mkdir -p /var/run/cube
 
 #copying scripts
-echo "\n\n====\n4. Copying start/stop scripts...\n====\n\n"
-cp cube-collector /etc/init.d/cube-collector
-cp cube-evaluator /etc/init.d/cube-evaluator
+cat << EOF
+
+-----------------------------
+4. Copying start/stop scripts
+-----------------------------
+
+EOF
+cp $SOURCE/cube-collector /etc/init.d/cube-collector
+cp $SOURCE/cube-evaluator /etc/init.d/cube-evaluator
 chmod +x /etc/init.d/cube-collector /etc/init.d/cube-evaluator
 
 #running daemons
-echo "\n\n====\n5. Starting daemons...\n====\n\n"
+cat << EOF
+
+------------------
+5. Running daemons
+------------------
+
+EOF
 /etc/init.d/cube-collector start
 /etc/init.d/cube-evaluator start
 
-echo "\n\n Done.\n"
+
+cat << EOF
+
+Done.
+
+EOF
